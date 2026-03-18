3a2de88af846ebce31241e0243a5118885961e63
Roman Krendyasov <krendyasov_rn@mail.ru>
2026-03-18 13:19:02 +0400

    Исправлены уязвимости: SQL injection и debug=True

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import sqlite3
+import os
 
 app = Flask(__name__)
 
-DB_PASSWORD = "P@ssw0rd"
+DB_PASSWORD = os.getenv("DB_PASSWORD", "P@ssw0rd")
 
 
 def get_db():
@@ -15,11 +16,15 @@ def get_user():
     name = request.args.get('name', '')
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE name = '" + name + "'")
+
+    cur.execute("SELECT * FROM users WHERE name = ?", (name, ))
     row = cur.fetchone()
+    conn.close
+
     if row:
-        return {"id": row[0], "name": row[1]}
-    return {"error": "User not found"}, 404
+        return jsonify({"id": row[0], "name": row[1]})
+    return jsonify({"error": "User not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+    app.run(debug=debug_mode)
